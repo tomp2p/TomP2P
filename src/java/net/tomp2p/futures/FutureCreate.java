a734321b79ea61eb684932dbcf9941ee6b0142f6
Thomas Bocek <tom@tomp2p.net>
2010-12-17 10:16:10 +0000

    maven fixes

--- src/java/net/tomp2p/futures/FutureCreate.java
+++
@@ -1,6 +0,0 @@
-package net.tomp2p.futures;
-
-public interface FutureCreate<K extends BaseFuture>
-{
-	public void repeated(K future);
-}
--- src/java/net/tomp2p/peers/PeerOfflineListener.java
+++
@@ -1,7 +0,0 @@
-package net.tomp2p.peers;
-public interface PeerOfflineListener
-{
-	public void peerOffline(PeerAddress peerAddress);
-
-	public void peerFail(PeerAddress peerAddress);
-}

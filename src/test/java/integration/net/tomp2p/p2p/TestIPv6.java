d36899891014da1fe2ac3b1359cf7911c2f65f9a
Thomas Bocek <tom@tomp2p.net>
2011-03-20 12:08:19 +0000

    removed header info

--- src/test/java/integration/net/tomp2p/p2p/TestIPv6.java
+++ src/test/java/integration/net/tomp2p/p2p/TestIPv6.java
@@ -1,17 +1,3 @@
-/*
- * @(#) $CVSHeader:  $
- *
- * Copyright (C) 2011 by Netcetera AG.
- * All rights reserved.
- *
- * The copyright to the computer program(s) herein is the property of
- * Netcetera AG, Switzerland.  The program(s) may be used and/or copied
- * only with the written permission of Netcetera AG or in accordance
- * with the terms and conditions stipulated in the agreement/contract
- * under which the program(s) have been supplied.
- *
- * @(#) $Id: codetemplates.xml,v 1.5 2004/06/29 12:49:49 hagger Exp $
- */
 package net.tomp2p.p2p;
 
 import java.net.InetSocketAddress;

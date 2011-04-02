672b93b13764b5dd49814ff07b54c4fbf49a118b
Thomas Bocek <tom@tomp2p.net>
2011-04-02 13:33:32 +0000

    changed license from gpl to apache

--- src/main/java/net/tomp2p/examples/Examples.java
+++ src/main/java/net/tomp2p/examples/Examples.java
@@ -1,31 +1,17 @@
 /*
- * This file is part of TomP2P.
+ * Copyright 2009 Thomas Bocek
  * 
- * TomP2P is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  * 
- * TomP2P is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with TomP2P. If not, see <http://www.gnu.org/licenses/>.
- */
-/**
- * 
- * TomP2P - A distributed multi map
- * 
- * TomP2P is a Java-based P2P network, which implements a distributed multi map.
- * It implements besides DHT operations (get and put), also add, remove and size
- * operations and further extensions. TomP2P was developed at the University of
- * Zurich, IFI, Communication Systems Group.
- * 
- * Copyright (C) 2009 University of Zurich, Thomas Bocek
- * 
- * @author Thomas Bocek
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package net.tomp2p.examples;
 import java.io.IOException;

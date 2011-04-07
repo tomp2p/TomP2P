5a30e44be5edae644fa2361670b877a9bbea3a36
Thomas Bocek <tom@tomp2p.net>
2011-04-07 22:46:24 +0000

    added better support for secondary trackers

--- src/main/java/net/tomp2p/storage/TrackerStorage.java
+++ src/main/java/net/tomp2p/storage/TrackerStorage.java
@@ -14,13 +14,17 @@
  * the License.
  */
 package net.tomp2p.storage;
-import java.io.IOException;
+
 import java.security.PublicKey;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 import java.util.SortedMap;
+import java.util.SortedSet;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,8 +32,10 @@ import org.slf4j.LoggerFactory;
 import net.tomp2p.peers.Number160;
 import net.tomp2p.peers.Number320;
 import net.tomp2p.peers.Number480;
+import net.tomp2p.peers.PeerAddress;
 import net.tomp2p.rpc.DigestInfo;
 import net.tomp2p.rpc.SimpleBloomFilter;
+import net.tomp2p.rpc.TrackerData;
 
 /**
  * The maintenance for the tracker is done by the client peer. Thus the peers on
@@ -47,10 +53,14 @@ public class TrackerStorage extends StorageMemory
 	// packet. This means that the attached data must be 0, otherwise you have
 	// to used tcp. don't forget to add the header as well
 	final private int trackerSize = 20;
-	private int trackerStoreSize = 40;
+	private int secondaryFactor = 5;
+	private int primanyFactor = 1;
+	final private Set<Number320> secondaryTracker=new HashSet<Number320>();
+	private int trackerStoreSize = trackerSize;
+	
 	final private Random rnd;
 	private volatile int trackerTimoutSeconds = Integer.MAX_VALUE;
-	
+
 	public TrackerStorage(long seed)
 	{
 		rnd = new Random(seed);
@@ -70,14 +80,28 @@ public class TrackerStorage extends StorageMemory
 	{
 		this.trackerTimoutSeconds = trackerTimoutSeconds;
 	}
+	
+	public void addAsSecondaryTracker(Number160 locationKey, Number160 domainKey)
+	{
+		secondaryTracker.add(new Number320(locationKey, domainKey));
+	}
 
 	public boolean put(Number160 locationKey, Number160 domainKey, PublicKey publicKey, Data data)
-			throws IOException
 	{
 		// fixed ttl, not set by the user
 		data.setTTLSeconds(getTrackerTimoutSeconds());
 		Number480 number480 = new Number480(locationKey, domainKey, data.getPeerAddress().getID());
-		return put(number480, data, publicKey, false, publicKey != null);
+		boolean retVal = put(number480, data, publicKey, false, publicKey != null);
+		//System.err.println("size22:"+get(new Number320(locationKey, domainKey)).size());
+		return retVal;
+	}
+
+	public boolean putReferred(Number160 locationKey, Number160 domainKey, Data data)
+	{
+		data.setTTLSeconds(getTrackerTimoutSeconds());
+		Number480 number480 = new Number480(locationKey, domainKey.xor(Number160.MAX_VALUE), data.getPeerAddress().getID());
+		//Number480 number480 = new Number480(locationKey, domainKey, data.getPeerAddress().getID());
+		return put(number480, data, null, false, false);
 	}
 
 	public int size(Number160 locationKey, Number160 domainKey)
@@ -86,39 +110,84 @@ public class TrackerStorage extends StorageMemory
 		return digest.getSize();
 	}
 
-	public SortedMap<Number480, Data> getSelection(Number320 number320, int nr, SimpleBloomFilter<Number160> knownPeers)
+	public TrackerData getSelection(Number160 locationKey, Number160 domainKey, int nr,
+			SimpleBloomFilter<Number160> knownPeers)
 	{
 		// we have a copy here, so no need to sync
+		Number320 number320 = new Number320(locationKey, domainKey);
 		List<Number480> keys = getKeys(number320);
 		// the peer is not interested in those peers! filter them
-		if(knownPeers!=null) 
+		if (knownPeers != null)
 		{
-			for(Iterator<Number480> i=keys.iterator();i.hasNext();)
+			for (Iterator<Number480> i = keys.iterator(); i.hasNext();)
 			{
-				Number480 number480=i.next();
-				if(knownPeers.contains(number480.getContentKey()))
+				Number480 number480 = i.next();
+				if (knownPeers.contains(number480.getContentKey()))
 				{
-					if(logger.isDebugEnabled())
-						logger.debug("We won't deliver "+number480.getContentKey()+" as the peer indicates that its already known.");
+					if (logger.isDebugEnabled())
+						logger.debug("We won't deliver " + number480.getContentKey()
+								+ " as the peer indicates that its already known.");
 					i.remove();
 				}
 			}
 		}
 		int size = keys.size();
+		//TODO: do I need a treemap here?
 		SortedMap<Number480, Data> result = new TreeMap<Number480, Data>();
 		for (int i = 0; i < nr && i < size; i++)
 		{
 			Number480 rndKey = keys.remove(rnd.nextInt(size - i));
 			result.put(rndKey, get(rndKey));
 		}
-		return result;
+		return new TrackerData(result, !keys.isEmpty());
+	}
+	
+	public SortedSet<PeerAddress> getSelectionSecondary(Number160 locationKey, Number160 domainKey, int nr)
+	{
+		TrackerData data= getSelection(locationKey, domainKey.xor(Number160.MAX_VALUE), nr, null);
+		SortedSet<PeerAddress> retVal=new TreeSet<PeerAddress>();
+		for(Data dat:data.getPeerDataMap().values())
+		{
+			retVal.add(dat.getPeerAddress());
+		}
+		return retVal;
+	}
+	
+	public boolean isSecondaryTracker(Number160 locationKey, Number160 domainKey)
+	{
+		return secondaryTracker.contains(new Number320(locationKey, domainKey));
 	}
 
-  public int getTrackerStoreSize() {
-    return trackerStoreSize;
-  }
+	public int getTrackerStoreSize(Number160 locationKey, Number160 domainKey)
+	{
+		if(isSecondaryTracker(locationKey, domainKey))
+			return trackerStoreSize * getSecondaryFactor();
+		else
+			return trackerStoreSize * getPrimanyFactor();
+	}
 
-  public void setTrackerStoreSize(int trackerStoreSize) {
-    this.trackerStoreSize = trackerStoreSize;
-  }
+	public void setTrackerStoreSize(int trackerStoreSize)
+	{
+		this.trackerStoreSize = trackerStoreSize;
+	}
+
+	public void setSecondaryFactor(int secondaryFactor)
+	{
+		this.secondaryFactor = secondaryFactor;
+	}
+
+	public int getSecondaryFactor()
+	{
+		return secondaryFactor;
+	}
+
+	public void setPrimanyFactor(int primanyFactor)
+	{
+		this.primanyFactor = primanyFactor;
+	}
+
+	public int getPrimanyFactor()
+	{
+		return primanyFactor;
+	}
 }

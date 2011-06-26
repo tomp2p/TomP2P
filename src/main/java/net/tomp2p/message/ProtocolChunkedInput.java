50823879edc0e22518006aa37d031850db9b0010
Thomas Bocek <tom@tomp2p.net>
2011-06-26 14:24:11 +0000

    changed license header

--- src/main/java/net/tomp2p/message/ProtocolChunkedInput.java
+++ src/main/java/net/tomp2p/message/ProtocolChunkedInput.java
@@ -1,16 +1,17 @@
 /*
- * @(#) $CVSHeader:  $
- *
- * Copyright (C) 2011 by Netcetera AG.
- * All rights reserved.
- *
- * The copyright to the computer program(s) herein is the property of
- * Netcetera AG, Switzerland.  The program(s) may be used and/or copied
- * only with the written permission of Netcetera AG or in accordance
- * with the terms and conditions stipulated in the agreement/contract
- * under which the program(s) have been supplied.
- *
- * @(#) $Id: codetemplates.xml,v 1.5 2004/06/29 12:49:49 hagger Exp $
+ * Copyright 2011 Thomas Bocek
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package net.tomp2p.message;
 

cd4e4611c034da9253cb71efcc63115574065604
Thomas Bocek <tom@tomp2p.net>
2012-01-31 02:07:53 +0100

    SimGrid integration

--- src/main/java/net/tomp2p/connection/Reservation.java
+++ src/main/java/net/tomp2p/connection/Reservation.java
@@ -16,9 +16,17 @@
 package net.tomp2p.connection;
 import java.util.concurrent.Semaphore;
 
+import net.tomp2p.futures.FutureRunnable;
+
 public interface Reservation
 {
 	public abstract void shutdown();
 
 	public abstract boolean acquire(Semaphore semaphore, int permits);
+
+	public abstract void runDeadLockProof(Scheduler scheduler, FutureRunnable futureRunnable);
+
+	public abstract void prepareDeadLockCheck();
+
+	public abstract void removeDeadLockCheck(long creatorThread);
 }

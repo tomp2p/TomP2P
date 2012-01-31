173b4a942672fb743afa15f9f1d35dec944f7ab0
Thomas Bocek <tom@tomp2p.net>
2012-01-31 02:11:51 +0100

    fixed maintenance and maintenece tests

--- src/test/java/net/tomp2p/p2p/TestMaintenance.java
+++ src/test/java/net/tomp2p/p2p/TestMaintenance.java
@@ -3,10 +3,9 @@ package net.tomp2p.p2p;
 import java.util.Collection;
 import java.util.Random;
 
-import net.tomp2p.p2p.Peer;
 import net.tomp2p.peers.Number160;
 import net.tomp2p.peers.PeerAddress;
-import net.tomp2p.utils.Utils;
+import net.tomp2p.utils.Timings;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -50,15 +49,18 @@ public class TestMaintenance
 	@Test
 	public void testMaintenance2() throws Exception
 	{
+		//Random rnd = new Random(42L);
 		Peer master = null;
 		try
 		{
 			master = new Peer(new Number160(rnd));
-			master.getP2PConfiguration().setStartMaintenance(false);
-			master.getP2PConfiguration().getWaitingTimeBetweenNodeMaintenenceSeconds()[0] = 0;
-			master.getP2PConfiguration().getWaitingTimeBetweenNodeMaintenenceSeconds()[1] = 4;
+			setTime(master, 0, 3);
 			master.listen(4001, 4001);
-			Peer[] nodes = createNodes(master, 500);
+			Peer[] nodes = createNodes(master, 500, rnd);
+			for(Peer peer:nodes)
+			{
+				setTime(peer, 0, 3);
+			}
 			// perfect routing
 			for (int i = 0; i < nodes.length; i++)
 			{
@@ -69,51 +71,17 @@ public class TestMaintenance
 					nodes[i].getPeerBean().getPeerMap().peerFound(nodes[j].getPeerAddress(), null);
 				}
 			}
+			System.err.println("routing done "+Timings.currentTimeMillis());
 			Collection<PeerAddress> pas = master.getPeerBean().getPeerMap().peersForMaintenance();
 			//this needs to be empty because its within the 4 seconds, where we do our second maintenance loop.
 			Assert.assertEquals(0, pas.size());
-			Utils.sleep(4000);
+			Timings.sleep(3000);
 			pas = master.getPeerBean().getPeerMap().peersForMaintenance();
 			//after 4 seconds we get all the peers back. 160 * bagsize is the maximum capacity
 			Assert.assertEquals(160 * master.getP2PConfiguration().getBagSize(), pas.size());
 		}
 		finally
 		{
-			master.shutdown();
-		}
-	}
-
-	@Test
-	public void testMaintenance3() throws Exception
-	{
-		Peer master = null;
-		try
-		{
-			master = new Peer(new Number160(rnd));
-			master.getP2PConfiguration().setStartMaintenance(false);
-			master.getP2PConfiguration().getWaitingTimeBetweenNodeMaintenenceSeconds()[0] = 1;
-			master.getP2PConfiguration().getWaitingTimeBetweenNodeMaintenenceSeconds()[1] = 3;
-			master.getP2PConfiguration().getWaitingTimeBetweenNodeMaintenenceSeconds()[2] = 3;
-			master.listen(4001, 4001);
-			Peer[] nodes = createNodes(master, 500);
-			// perfect routing
-			for (int i = 0; i < nodes.length; i++)
-			{
-				master.getPeerBean().getPeerMap().peerFound(nodes[i].getPeerAddress(), null);
-				nodes[i].getPeerBean().getPeerMap().peerFound(master.getPeerAddress(), null);
-				for (int j = 0; j < nodes.length; j++)
-				{
-					nodes[i].getPeerBean().getPeerMap().peerFound(nodes[j].getPeerAddress(), null);
-				}
-			}
-			//
-			Utils.sleep(3000);
-			master.startMaintainance();
-			Utils.sleep(10000);
-		}
-		finally
-		{
-			System.err.println("Shutdown!!");
 			master.shutdown();
 		}
 	}
@@ -128,10 +96,10 @@ public class TestMaintenance
 			setTime(master);
 			master.listen(4001, 4001);
 			Peer[] nodes = createNodes(master, 4);
-			setTime(nodes[0]);
-			setTime(nodes[1]);
-			setTime(nodes[2]);
-			setTime(nodes[3]);
+			setTime(nodes[0], 1, 1, 1, 1, 1, 1);
+			setTime(nodes[1], 1, 1, 1, 1, 1, 1);
+			setTime(nodes[2], 1, 1, 1, 1, 1, 1);
+			setTime(nodes[3], 1, 1, 1, 1, 1, 1);
 			// perfect routing
 			for (int i = 0; i < nodes.length; i++)
 			{
@@ -143,16 +111,16 @@ public class TestMaintenance
 				}
 			}
 			//
-			Utils.sleep(3000);
+			Timings.sleep(3000);
 			master.startMaintainance();
 			nodes[0].startMaintainance();
 			nodes[1].startMaintainance();
 			nodes[2].startMaintainance();
 			nodes[3].startMaintainance();
-			Utils.sleep(10000);
+			Timings.sleep(10000);
 			PeerAddress node3 = nodes[3].getPeerAddress();
 			nodes[3].shutdown();
-			Utils.sleep(15000);
+			Timings.sleep(15000);
 
 			Assert.assertEquals(false, master.getPeerBean().getPeerMap().contains(node3));
 			Assert.assertEquals(false, nodes[0].getPeerBean().getPeerMap().contains(node3));
@@ -166,18 +134,21 @@ public class TestMaintenance
 		}
 	}
 
-	private void setTime(Peer master)
+	private void setTime(Peer peer, int... times)
 	{
-		master.getP2PConfiguration().setStartMaintenance(false);
-		master.getP2PConfiguration().getWaitingTimeBetweenNodeMaintenenceSeconds()[0] = 1;
-		master.getP2PConfiguration().getWaitingTimeBetweenNodeMaintenenceSeconds()[1] = 1;
-		master.getP2PConfiguration().getWaitingTimeBetweenNodeMaintenenceSeconds()[2] = 1;
-		master.getP2PConfiguration().getWaitingTimeBetweenNodeMaintenenceSeconds()[3] = 1;
-		master.getP2PConfiguration().getWaitingTimeBetweenNodeMaintenenceSeconds()[4] = 1;
-		master.getP2PConfiguration().getWaitingTimeBetweenNodeMaintenenceSeconds()[5] = 1;
+		peer.getP2PConfiguration().setStartMaintenance(false);
+		for(int i=0;i<times.length;i++)
+		{
+			peer.getP2PConfiguration().getWaitingTimeBetweenNodeMaintenenceSeconds()[i] = times[i];
+		}
+	}
+	
+	private Peer[] createNodes(Peer master, int nr) throws Exception
+	{
+		return createNodes(master, nr, rnd);
 	}
 
-	private Peer[] createNodes(Peer master, int nr) throws Exception
+	private Peer[] createNodes(Peer master, int nr, Random rnd) throws Exception
 	{
 		Peer[] nodes = new Peer[nr];
 		for (int i = 0; i < nr; i++)

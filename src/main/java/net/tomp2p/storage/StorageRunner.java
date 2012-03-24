a32a4338bd546a5ba7728ae832b7fcd9ce8a34c4
tbocek <tom@tomp2p.net>
2012-03-24 03:25:34 +0100

    removed unused classes

--- src/main/java/net/tomp2p/storage/StorageRunner.java
+++
@@ -1,8 +0,0 @@
-package net.tomp2p.storage;
-import net.tomp2p.peers.Number160;
-
-public interface StorageRunner
-{
-	public abstract void call(Number160 locationKey, Number160 domainKey, Number160 contentKey,
-			Data data);
-}

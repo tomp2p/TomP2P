1e0087a1759dd7e9152deda140872de005bf84ff
tbocek <tom@tomp2p.net>
2012-06-11 08:38:54 +0200

    refactored add tracker

--- src/main/java/net/tomp2p/p2p/builder/AddTrackerBuilder.java
+++ src/main/java/net/tomp2p/p2p/builder/AddTrackerBuilder.java
@@ -3,17 +3,16 @@ package net.tomp2p.p2p.builder;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
-import org.hamcrest.core.IsSame;
-
 import net.tomp2p.futures.BaseFuture;
+import net.tomp2p.futures.Cancellable;
 import net.tomp2p.futures.FutureChannelCreator;
+import net.tomp2p.futures.FutureCleanup;
 import net.tomp2p.futures.FutureCreate;
-import net.tomp2p.futures.FutureDHT;
+import net.tomp2p.futures.FutureCreator;
 import net.tomp2p.futures.FutureLateJoin;
 import net.tomp2p.futures.FutureResponse;
 import net.tomp2p.futures.FutureTracker;
 import net.tomp2p.p2p.Peer;
-import net.tomp2p.p2p.config.ConfigurationTrackerStore;
 import net.tomp2p.peers.Number160;
 import net.tomp2p.rpc.SimpleBloomFilter;
 import net.tomp2p.storage.TrackerStorage;
@@ -25,11 +24,59 @@ public class AddTrackerBuilder extends TrackerBuilder<AddTrackerBuilder>
 	private int pexWaitSec = 0;
 	private SimpleBloomFilter<Number160> bloomFilter;
 	private FutureCreate<BaseFuture> futureCreate;
-	private boolean messageSign;
+	private FutureCreator<FutureTracker> defaultDirectReplication;
+	private FutureCreator<FutureLateJoin<FutureResponse>> defaultPEX;
+	private boolean messageSign = false;
+	private boolean tcpPEX = false;
+
 	public AddTrackerBuilder(Peer peer, Number160 locationKey)
 	{
 		super(peer, locationKey);
 		self(this);
+	}
+	
+	public byte[] getAttachement()
+	{
+		return attachement;
+	}
+
+	public AddTrackerBuilder setAttachement(byte[] attachement)
+	{
+		this.attachement = attachement;
+		return this;
+	}
+
+	public int getTrackerTimeoutSec()
+	{
+		return trackerTimeoutSec;
+	}
+
+	public AddTrackerBuilder setTrackerTimeoutSec(int trackerTimeoutSec)
+	{
+		this.trackerTimeoutSec = trackerTimeoutSec;
+		return this;
+	}
+
+	public int getPexWaitSec()
+	{
+		return pexWaitSec;
+	}
+
+	public AddTrackerBuilder setPexWaitSec(int pexWaitSec)
+	{
+		this.pexWaitSec = pexWaitSec;
+		return this;
+	}
+
+	public SimpleBloomFilter<Number160> getBloomFilter()
+	{
+		return bloomFilter;
+	}
+
+	public AddTrackerBuilder setBloomFilter(SimpleBloomFilter<Number160> bloomFilter)
+	{
+		this.bloomFilter = bloomFilter;
+		return this;
 	}
 	
 	public FutureCreate<BaseFuture> getFutureCreate()
@@ -42,13 +89,69 @@ public class AddTrackerBuilder extends TrackerBuilder<AddTrackerBuilder>
 		this.futureCreate = futureCreate;
 		return this;
 	}
+
+	public FutureCreator<FutureTracker> getDefaultDirectReplication()
+	{
+		return defaultDirectReplication;
+	}
+
+	public AddTrackerBuilder setDefaultDirectReplication(FutureCreator<FutureTracker> defaultDirectReplication)
+	{
+		this.defaultDirectReplication = defaultDirectReplication;
+		return this;
+	}
+
+	public FutureCreator<FutureLateJoin<FutureResponse>> getDefaultPEX()
+	{
+		return defaultPEX;
+	}
+
+	public AddTrackerBuilder setDefaultPEX(FutureCreator<FutureLateJoin<FutureResponse>> defaultPEX)
+	{
+		this.defaultPEX = defaultPEX;
+		return this;
+	}
+
+	public boolean isMessageSign()
+	{
+		return messageSign;
+	}
 	
+	public AddTrackerBuilder setMessageSign()
+	{
+		this.messageSign = true;
+		return this;
+	}
+
+	public AddTrackerBuilder setMessageSign(boolean messageSign)
+	{
+		this.messageSign = messageSign;
+		return this;
+	}
+
+	public boolean isTcpPEX()
+	{
+		return tcpPEX;
+	}
+	
+	public AddTrackerBuilder setTcpPEX()
+	{
+		this.tcpPEX = true;
+		return this;
+	}
+
+	public AddTrackerBuilder setTcpPEX(boolean tcpPEX)
+	{
+		this.tcpPEX = tcpPEX;
+		return this;
+	}
+
 	@Override
 	public FutureTracker build()
 	{
 		preBuild("add-tracker-build");
-		
-		if(bloomFilter == null)
+
+		if (bloomFilter == null)
 		{
 			bloomFilter = new SimpleBloomFilter<Number160>(1024, 1024);
 		}
@@ -60,78 +163,103 @@ public class AddTrackerBuilder extends TrackerBuilder<AddTrackerBuilder>
 				domainKey,
 				attachement, routingConfiguration,
 				trackerConfiguration,
-				messageSign, futureCreate, bloomFilter, futureChannelCreator, 
+				messageSign, futureCreate, bloomFilter, futureChannelCreator,
 				peer.getConnectionBean().getConnectionReservation());
-		
+
 		if (trackerTimeoutSec > 0)
 		{
-			final ScheduledFuture<?> tmp = scheduleAddTracker(locationKey, config, futureTracker);
-			setupCancel(futureTracker, tmp);
-		}
-		if (pexWaitSec > 0)
-		{
-			final ScheduledFuture<?> tmp = schedulePeerExchange(locationKey, config, futureTracker);
-			setupCancel(futureTracker, tmp);
-		}
-		return futureTracker;
-	
-	}
-	
-	private ScheduledFuture<?> scheduleAddTracker(final Number160 locationKey,
-			final ConfigurationTrackerStore config,
-			final FutureTracker futureTracker)
-	{
-		Runnable runner = new Runnable()
-		{
-			@Override
-			public void run()
+			if (defaultDirectReplication == null)
 			{
-				// make a good guess based on the config and the maxium tracker
-				// that can be found
-				SimpleBloomFilter<Number160> bloomFilter = new SimpleBloomFilter<Number160>(
-						BLOOMFILTER_SIZE, 1024);
-				int conn = Math.max(config.getRoutingConfiguration().getParallel(), config
-						.getTrackerConfiguration().getParallel());
-				final FutureChannelCreator futureChannelCreator = getConnectionBean().getConnectionReservation().reserve(conn);
-
-				FutureTracker futureTracker2 = getDistributedTracker().addToTracker(locationKey,
-						config.getDomain(), config.getAttachement(),
-						config.getRoutingConfiguration(), config.getTrackerConfiguration(),
-						config.isSignMessage(), config.getFutureCreate(), bloomFilter,
-						futureChannelCreator, getConnectionBean().getConnectionReservation());
-				futureTracker.repeated(futureTracker2);
+				defaultDirectReplication = new DefaultDirectReplication();
 			}
-		};
-		int refresh = getPeerBean().getTrackerStorage().getTrackerTimoutSeconds() * 3 / 4;
-		ScheduledFuture<?> tmp = getConnectionBean().getScheduler().getScheduledExecutorServiceReplication().scheduleAtFixedRate(runner,
-				refresh, refresh,
-				TimeUnit.SECONDS);
-		scheduledFutures.add(tmp);
-		return tmp;
-	}	
-	
-	//TODO: enable this via AddBuilder
-		public ScheduledFuture<?> schedulePeerExchange(final Number160 locationKey,
-				final ConfigurationTrackerStore config, final FutureTracker futureTracker)
-		{
 			Runnable runner = new Runnable()
 			{
 				@Override
 				public void run()
 				{
-					final FutureChannelCreator futureChannelCreator = getConnectionBean().getConnectionReservation().reserve(
-							TrackerStorage.TRACKER_SIZE);
-					FutureLateJoin<FutureResponse> futureLateJoin = getDistributedTracker().startPeerExchange(
-							locationKey, config.getDomain(), futureChannelCreator, 
-							getConnectionBean().getConnectionReservation(), config.getTrackerConfiguration().isForceTCP());
-					futureTracker.repeated(futureLateJoin);
+					FutureTracker futureTracker2 = defaultDirectReplication.create();
+					futureTracker.repeated(futureTracker2);
 				}
 			};
-			int refresh = config.getWaitBeforeNextSendSeconds();
-			ScheduledFuture<?> tmp = getConnectionBean().getScheduler().getScheduledExecutorServiceReplication().scheduleAtFixedRate(runner,
-					refresh, refresh,
-					TimeUnit.SECONDS);
-			scheduledFutures.add(tmp);
-			return tmp;
+			ScheduledFuture<?> tmp = peer
+					.getConnectionBean()
+					.getScheduler()
+					.getScheduledExecutorServiceReplication()
+					.scheduleAtFixedRate(
+							runner, trackerTimeoutSec * (2 / 3), trackerTimeoutSec * (2 / 3),
+							TimeUnit.SECONDS);
+			setupCancel(futureTracker, tmp);
 		}
+		if (pexWaitSec > 0)
+		{
+			if (defaultPEX == null)
+			{
+				defaultPEX = new DefaultPEX();
+			}
+			Runnable runner = new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					FutureLateJoin<FutureResponse> future = defaultPEX.create();
+					futureTracker.repeated(future);
+				}
+			};
+			ScheduledFuture<?> tmp = peer.getConnectionBean().getScheduler()
+					.getScheduledExecutorServiceReplication().scheduleAtFixedRate(runner,
+							pexWaitSec, pexWaitSec, TimeUnit.SECONDS);
+			setupCancel(futureTracker, tmp);
+		}
+		return futureTracker;
+	}
+
+	protected void setupCancel(final FutureCleanup futureCleanup, final ScheduledFuture<?> future)
+	{
+		peer.getScheduledFutures().add(future);
+		futureCleanup.addCleanup(new Cancellable()
+		{
+			@Override
+			public void cancel()
+			{
+				future.cancel(true);
+				peer.getScheduledFutures().remove(future);
+			}
+		});
+	}
+
+	private class DefaultDirectReplication implements FutureCreator<FutureTracker>
+	{
+		@Override
+		public FutureTracker create()
+		{
+			int conn = Math.max(routingConfiguration.getParallel(),
+					trackerConfiguration.getParallel());
+			final FutureChannelCreator futureChannelCreator = peer.getConnectionBean()
+					.getConnectionReservation().reserve(conn);
+			final FutureTracker futureTracker = peer.getDistributedTracker().addToTracker(
+					locationKey,
+					domainKey,
+					getAttachement(), routingConfiguration,
+					trackerConfiguration,
+					isMessageSign(), futureCreate, getBloomFilter(), futureChannelCreator,
+					peer.getConnectionBean().getConnectionReservation());
+			return futureTracker;
+		}
+	}
+
+	private class DefaultPEX implements FutureCreator<FutureLateJoin<FutureResponse>>
+	{
+		@Override
+		public FutureLateJoin<FutureResponse> create()
+		{
+			final FutureChannelCreator futureChannelCreator = peer.getConnectionBean()
+					.getConnectionReservation().reserve(
+							TrackerStorage.TRACKER_SIZE);
+			FutureLateJoin<FutureResponse> futureLateJoin = peer.getDistributedTracker()
+					.startPeerExchange(
+							locationKey, domainKey, futureChannelCreator,
+							peer.getConnectionBean().getConnectionReservation(), tcpPEX);
+			return futureLateJoin;
+		}
+	}
 }

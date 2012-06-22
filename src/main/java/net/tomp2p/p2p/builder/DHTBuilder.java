3ffa6ceb6e8e09ad3905835b264482f07243f0a8
tbocek <tom@tomp2p.net>
2012-06-22 20:32:24 +0200

    fixed passive replication, a flag was missing

--- src/main/java/net/tomp2p/p2p/builder/DHTBuilder.java
+++ src/main/java/net/tomp2p/p2p/builder/DHTBuilder.java
@@ -223,6 +223,10 @@ public abstract class DHTBuilder<K extends DHTBuilder<K>>
 		{
 			futureChannelCreator = peer.reserve(routingConfiguration, requestP2PConfiguration, name);
 		}
+		if(refreshSeconds > 0)
+		{
+			directReplication = true;
+		}
 	}
 	
 	public abstract FutureDHT build();

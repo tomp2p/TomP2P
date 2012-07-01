a6d4e230741b85883b91f7e3c233aac367c646ae
Thomas Bocek <tom@tomp2p.net>
2012-07-01 19:45:42 +0200

    cleanup for release

---
+++ src/main/java/net/tomp2p/p2p/builder/BroadcastBuilder.java
@@ -0,0 +1,13 @@
+package net.tomp2p.p2p.builder;
+
+import net.tomp2p.p2p.Peer;
+import net.tomp2p.peers.Number160;
+
+public class BroadcastBuilder
+{
+
+	public BroadcastBuilder(Peer peer, Number160 key)
+	{
+		// TODO Auto-generated constructor stub
+	}
+}

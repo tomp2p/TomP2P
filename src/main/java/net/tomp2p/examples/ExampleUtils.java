48f373c37c0a0f4971a16b5d4349ef1c0b9408c8
tbocek <tom@tomp2p.net>
2012-11-18 12:09:19 +0100

    add a new example

--- src/main/java/net/tomp2p/examples/ExampleUtils.java
+++ src/main/java/net/tomp2p/examples/ExampleUtils.java
@@ -15,6 +15,7 @@
  */
 package net.tomp2p.examples;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -32,8 +33,13 @@ import net.tomp2p.peers.Number160;
  */
 public class ExampleUtils
 {
-    final private static Random rnd = new Random( 42L );
+    private static final Random RND = new Random( 42L );
 
+    /**
+     * Bootstraps peers to the first peer in the array.
+     * 
+     * @param peers The peers that should be bootstrapped
+     */
     public static void bootstrap( Peer[] peers )
     {
         List<FutureBootstrap> futures1 = new ArrayList<FutureBootstrap>();
@@ -58,22 +64,31 @@ public class ExampleUtils
             futures1.add( tmp );
         }
         for ( FutureBootstrap future : futures1 )
+        {
             future.awaitUninterruptibly();
+        }
     }
 
-    public static Peer[] createAndAttachNodes( int nr, int port )
-        throws Exception
+    /**
+     * Create peers with a port and attach it to the first peer in the array.
+     * 
+     * @param nr The number of peers to be created
+     * @param port The port that all the peer listens to. The multiplexing is done via the peer Id
+     * @return The created peers
+     * @throws IOException IOException
+     */
+    public static Peer[] createAndAttachNodes( int nr, int port ) throws IOException
     {
         Peer[] peers = new Peer[nr];
         for ( int i = 0; i < nr; i++ )
         {
             if ( i == 0 )
             {
-                peers[0] = new PeerMaker( new Number160( rnd ) ).setPorts( port ).makeAndListen();
+                peers[0] = new PeerMaker( new Number160( RND ) ).setPorts( port ).makeAndListen();
             }
             else
             {
-                peers[i] = new PeerMaker( new Number160( rnd ) ).setMasterPeer( peers[0] ).makeAndListen();
+                peers[i] = new PeerMaker( new Number160( RND ) ).setMasterPeer( peers[0] ).makeAndListen();
             }
         }
         return peers;

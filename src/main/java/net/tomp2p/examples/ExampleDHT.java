e69ce437a993cab0a9ccc199a0def1a2c6b56cea
tom <tom@tomp2p.net>
2013-03-25 17:44:18 +0100

    added javadoc and removed warnings

--- src/main/java/net/tomp2p/examples/ExampleDHT.java
+++ src/main/java/net/tomp2p/examples/ExampleDHT.java
@@ -16,140 +16,114 @@ import net.tomp2p.rpc.ObjectDataReply;
 import net.tomp2p.storage.Data;
 import net.tomp2p.storage.TrackerData;
 
-public class ExampleDHT
-{
-    public static void main( String[] args )
-        throws Exception
-    {
+public class ExampleDHT {
+    public static void main(String[] args) throws Exception {
         Peer[] peers = null;
-        try
-        {
-            peers = ExampleUtils.createAndAttachNodes( 100, 4001 );
-            ExampleUtils.bootstrap( peers );
-            MyPeer[] myPeers = wrap( peers );
-            example( myPeers );
-        }
-        finally
-        {
+        try {
+            peers = ExampleUtils.createAndAttachNodes(100, 4001);
+            ExampleUtils.bootstrap(peers);
+            MyPeer[] myPeers = wrap(peers);
+            example(myPeers);
+        } finally {
             // 0 is the master
             peers[0].shutdown();
         }
     }
 
-    private static MyPeer[] wrap( Peer[] peers )
-    {
+    private static MyPeer[] wrap(Peer[] peers) {
         MyPeer[] retVal = new MyPeer[peers.length];
-        for ( int i = 0; i < peers.length; i++ )
-        {
-            retVal[i] = new MyPeer( peers[i] );
+        for (int i = 0; i < peers.length; i++) {
+            retVal[i] = new MyPeer(peers[i]);
         }
         return retVal;
     }
 
-    private static void example( MyPeer[] peers )
-        throws IOException, ClassNotFoundException
-    {
+    private static void example(MyPeer[] peers) throws IOException, ClassNotFoundException {
         // 3 peers have files
-        System.out.println( "Setup: we have " + peers.length
-            + " peers; peers[12] has Song A, peers[24] has Song B, peers[42] has Song C" );
-        peers[12].announce( "Song A" );
-        peers[24].announce( "Song B" );
-        peers[42].announce( "Song C" );
+        System.out.println("Setup: we have " + peers.length
+                + " peers; peers[12] has Song A, peers[24] has Song B, peers[42] has Song C");
+        peers[12].announce("Song A");
+        peers[24].announce("Song B");
+        peers[42].announce("Song C");
         // peer 12 now searches for Song B
-        System.out.println( "peers[12] wants to download Song B" );
-        String downloaded = peers[12].download( Number160.createHash( "Song B" ) );
-        System.out.println( "peers[12] got " + downloaded );
+        System.out.println("peers[12] wants to download Song B");
+        String downloaded = peers[12].download(Number160.createHash("Song B"));
+        System.out.println("peers[12] got " + downloaded);
         // now peer 42 is also interested in song 24 and wants to know what other songs people listen to that downloaded
         // song 12
-        System.out.println( "peers[42] wants to download Song B" );
-        downloaded = peers[42].download( Number160.createHash( "Song B" ) );
-        System.out.println( "peers[42] got " + downloaded );
+        System.out.println("peers[42] wants to download Song B");
+        downloaded = peers[42].download(Number160.createHash("Song B"));
+        System.out.println("peers[42] got " + downloaded);
         // now peer 12 downloads again the same song
-        System.out.println( "peers[12] wants to download Song B" );
-        downloaded = peers[12].download( Number160.createHash( "Song B" ) );
-        System.out.println( "peers[12] got " + downloaded );
+        System.out.println("peers[12] wants to download Song B");
+        downloaded = peers[12].download(Number160.createHash("Song B"));
+        System.out.println("peers[12] got " + downloaded);
     }
 
-    private static class MyPeer
-    {
+    private static class MyPeer {
         final private Peer peer;
 
         final private Map<Number160, String> downloaded = new HashMap<Number160, String>();
 
-        public MyPeer( Peer peer )
-        {
+        public MyPeer(Peer peer) {
             this.peer = peer;
-            setReplyHandler( peer );
+            setReplyHandler(peer);
         }
 
-        public void announce( String title )
-            throws IOException
-        {
-            downloaded.put( Number160.createHash( title ), title );
+        public void announce(String title) throws IOException {
+            downloaded.put(Number160.createHash(title), title);
             announce();
         }
 
-        public void announce()
-            throws IOException
-        {
-            for ( Map.Entry<Number160, String> entry : downloaded.entrySet() )
-            {
-                peer.addTracker( entry.getKey() ).start().awaitUninterruptibly();
+        public void announce() throws IOException {
+            for (Map.Entry<Number160, String> entry : downloaded.entrySet()) {
+                peer.addTracker(entry.getKey()).start().awaitUninterruptibly();
                 // announce it on DHT
-                Collection<String> tmp = new ArrayList<String>( downloaded.values() );
-                tmp.remove( entry.getValue() );
+                Collection<String> tmp = new ArrayList<String>(downloaded.values());
+                tmp.remove(entry.getValue());
                 Map<Number160, Data> dataMap = new HashMap<Number160, Data>();
-                for ( String song : tmp )
-                {
-                    dataMap.put( peer.getPeerID().xor( Number160.createHash( song ) ), new Data( song ) );
+                for (String song : tmp) {
+                    dataMap.put(peer.getPeerID().xor(Number160.createHash(song)), new Data(song));
                 }
-                peer.put( entry.getKey() ).setDataMap( dataMap ).start().awaitUninterruptibly();
+                peer.put(entry.getKey()).setDataMap(dataMap).start().awaitUninterruptibly();
             }
         }
 
-        public String download( Number160 key )
-            throws IOException, ClassNotFoundException
-        {
-            FutureTracker futureTracker = peer.getTracker( key ).start();
+        public String download(Number160 key) throws IOException, ClassNotFoundException {
+            FutureTracker futureTracker = peer.getTracker(key).start();
             // now we know which peer has this data, and we also know what other things this peer has
             futureTracker.awaitUninterruptibly();
             Collection<TrackerData> trackerDatas = futureTracker.getTrackers();
-            for ( TrackerData trackerData : trackerDatas )
-            {
-                System.out.println( "Peer " + trackerData + " claims to have the content" );
+            for (TrackerData trackerData : trackerDatas) {
+                System.out.println("Peer " + trackerData + " claims to have the content");
             }
-            System.out.println( "Tracker reports that " + trackerDatas.size() + " peer(s) have this song" );
+            System.out.println("Tracker reports that " + trackerDatas.size() + " peer(s) have this song");
             // here we download
-            FutureResponse futureData =
-                peer.sendDirect(trackerDatas.iterator().next().getPeerAddress()).setObject( key ).start();
+            FutureResponse futureData = peer.sendDirect(trackerDatas.iterator().next().getPeerAddress()).setObject(key)
+                    .start();
             futureData.awaitUninterruptibly();
             String downloaded = (String) futureData.getObject();
             // get the recommondation
-            FutureDHT futureDHT = peer.get( key ).setAll().start();
+            FutureDHT futureDHT = peer.get(key).setAll().start();
             futureDHT.awaitUninterruptibly();
-            for ( Map.Entry<Number160, Data> entry : futureDHT.getDataMap().entrySet() )
-            {
-                System.out.println( "peers that downloaded this song also downloaded " + entry.getValue().getObject() );
+            for (Map.Entry<Number160, Data> entry : futureDHT.getDataMap().entrySet()) {
+                System.out.println("peers that downloaded this song also downloaded " + entry.getValue().getObject());
             }
             // we need to announce that we have this piece now
-            announce( downloaded );
+            announce(downloaded);
             return downloaded;
         }
 
-        private void setReplyHandler( Peer peer )
-        {
-            peer.setObjectDataReply( new ObjectDataReply()
-            {
+        private void setReplyHandler(Peer peer) {
+            peer.setObjectDataReply(new ObjectDataReply() {
                 @Override
-                public Object reply( PeerAddress sender, Object request )
-                    throws Exception
-                {
-                    if ( request != null && request instanceof Number160 )
-                        return downloaded.get( (Number160) request );
+                public Object reply(PeerAddress sender, Object request) throws Exception {
+                    if (request != null && request instanceof Number160)
+                        return downloaded.get((Number160) request);
                     else
                         return null;
                 }
-            } );
+            });
         }
     }
 }
--- src/main/java/net/tomp2p/examples/ExampleIndirectReplication.java
+++ src/main/java/net/tomp2p/examples/ExampleIndirectReplication.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 Thomas Bocek
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package net.tomp2p.examples;
 
 import java.io.IOException;
@@ -8,44 +24,78 @@ import net.tomp2p.p2p.PeerMaker;
 import net.tomp2p.peers.Number160;
 import net.tomp2p.storage.Data;
 
-public class ExampleIndirectReplication
-{
-    public static void main( String[] args )
-        throws Exception
-    {
+/**
+ * Example of indirect replication with put.
+ * 
+ * @author Thomas Bocek
+ * 
+ */
+public final class ExampleIndirectReplication {
+
+    private static final int ONE_SECOND = 1000;
+
+    /**
+     * Empty constructor.
+     */
+    private ExampleIndirectReplication() {
+    }
+
+    /**
+     * Create 3 peers and start the example.
+     * 
+     * @param args
+     *            Empty
+     * @throws Exception .
+     */
+    public static void main(final String[] args) throws Exception {
         exmpleIndirectReplication();
     }
 
-    private static void exmpleIndirectReplication()
-        throws IOException, InterruptedException
-    {
-        Peer peer1 =
-            new PeerMaker( new Number160( 1 ) ).setPorts( 4001 ).setEnableIndirectReplication( true ).makeAndListen();
-        Peer peer2 =
-            new PeerMaker( new Number160( 2 ) ).setPorts( 4002 ).setEnableIndirectReplication( true ).makeAndListen();
-        Peer peer3 =
-            new PeerMaker( new Number160( 4 ) ).setPorts( 4003 ).setEnableIndirectReplication( true ).makeAndListen();
-        Peer[] peers = new Peer[] { peer1, peer2, peer3 };
+    /**
+     * Example of indirect replication with put. We store data in the DHT, then peers join that are closer to this data.
+     * The indirect replication moves the content to the close peers.
+     * 
+     * @throws IOException .
+     * @throws InterruptedException .
+     */
+    private static void exmpleIndirectReplication() throws IOException, InterruptedException {
+        final int port1 = 4001;
+        final int nr1 = 1;
+        final int port2 = 4002;
+        final int nr2 = 2;
+        final int port3 = 4003;
+        final int nr3 = 4;
+        Peer peer1 = new PeerMaker(new Number160(nr1)).setPorts(port1).setEnableIndirectReplication(true)
+                .makeAndListen();
+        Peer peer2 = new PeerMaker(new Number160(nr2)).setPorts(port2).setEnableIndirectReplication(true)
+                .makeAndListen();
+        Peer peer3 = new PeerMaker(new Number160(nr3)).setPorts(port3).setEnableIndirectReplication(true)
+                .makeAndListen();
+        Peer[] peers = new Peer[] {peer1, peer2, peer3};
         //
-        FutureDHT futureDHT = peer1.put( new Number160( 4 ) ).setData( new Data( "store on peer1" ) ).start();
+        FutureDHT futureDHT = peer1.put(new Number160(nr3)).setData(new Data("store on peer1")).start();
         futureDHT.awaitUninterruptibly();
-        futureDHT = peer1.get( new Number160( 4 ) ).setDigest().start();
+        futureDHT = peer1.get(new Number160(nr3)).setDigest().start();
         futureDHT.awaitUninterruptibly();
-        System.out.println( "we found the data on " + futureDHT.getRawDigest().size() + " peers" );
+        System.out.println("we found the data on " + futureDHT.getRawDigest().size() + " peers");
         // now peer1 gets to know peer2, transfer the data
-        peer1.bootstrap().setPeerAddress( peer2.getPeerAddress() ).start();
-        peer1.bootstrap().setPeerAddress( peer3.getPeerAddress() ).start();
-        Thread.sleep( 1000 );
-        futureDHT = peer1.get( new Number160( 4 ) ).setDigest().start();
+        peer1.bootstrap().setPeerAddress(peer2.getPeerAddress()).start();
+        peer1.bootstrap().setPeerAddress(peer3.getPeerAddress()).start();
+        Thread.sleep(ONE_SECOND);
+        futureDHT = peer1.get(new Number160(nr3)).setDigest().start();
         futureDHT.awaitUninterruptibly();
-        System.out.println( "we found the data on " + futureDHT.getRawDigest().size() + " peers" );
-        shutdown( peers );
+        System.out.println("we found the data on " + futureDHT.getRawDigest().size() + " peers");
+        shutdown(peers);
     }
 
-    private static void shutdown( Peer[] peers )
-    {
-        for ( Peer peer : peers )
-        {
+    /**
+     * Shutdown all the peers.
+     * 
+     * @param peers
+     *            The peers in this P2P network
+     */
+    private static void shutdown(final Peer[] peers) {
+        for (Peer peer : peers) {
             peer.shutdown();
         }
     }

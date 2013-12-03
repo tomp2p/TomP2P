77ad456fe427c67c527945c7aab268c6eb80318a
Raphael Voellmy <r.voellmy@gmail.com>
2013-12-03 17:49:38 +0100

    many changes and additions

--- nat/src/main/java/relay/RelayConnections.java
+++ nat/src/main/java/relay/RelayConnections.java
@@ -2,18 +2,34 @@ package relay;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+import net.tomp2p.futures.FuturePeerConnection;
+
 public class RelayConnections {
 	
 	private int maxRelays;
 	private AtomicInteger connectionCount;
+	private AtomicInteger establishedConnectionCount;
 	
 	public RelayConnections(int maxRelays) {
 		this.maxRelays = maxRelays;
 		connectionCount = new AtomicInteger();
+		establishedConnectionCount = new AtomicInteger();
 	}
 	
 	public AtomicInteger connectionCount() {
 		return connectionCount;
+	}
+	
+	public AtomicInteger establishedConnectionCound() {
+		return establishedConnectionCount;
+	}
+	
+	public void connectionEstablished() {
+		establishedConnectionCount.getAndIncrement();
+	}
+	
+	public void connectionLost() {
+		establishedConnectionCount.getAndDecrement();
 	}
 	
 }
--- nat/src/main/java/relay/RelayRPC.java
+++ nat/src/main/java/relay/RelayRPC.java
@@ -5,6 +5,8 @@ import net.tomp2p.connection.ConnectionConfiguration;
 import net.tomp2p.connection.DefaultConnectionConfiguration;
 import net.tomp2p.connection.PeerConnection;
 import net.tomp2p.connection.RequestHandler;
+import net.tomp2p.connection.Dispatcher.Responder;
+import net.tomp2p.futures.BaseFutureListener;
 import net.tomp2p.futures.FuturePeerConnection;
 import net.tomp2p.futures.FutureResponse;
 import net.tomp2p.message.Message;
@@ -20,7 +22,7 @@ public class RelayRPC extends DispatchHandler {
 
 	public static final byte RELAY_COMMAND = 77;
 
-	private static final Logger LOG = LoggerFactory.getLogger(RelayRPC.class);
+	private static final Logger logger = LoggerFactory.getLogger(RelayRPC.class);
 	private ConnectionConfiguration config;
 	
 	private Peer peer;
@@ -31,33 +33,47 @@ public class RelayRPC extends DispatchHandler {
 		config = new DefaultConnectionConfiguration();
 	}
 
-	public FutureResponse setupRelay(PeerAddress other, final ChannelCreator channelCreator) {
+	public RelayConnectionFuture setupRelay(final PeerAddress other, final ChannelCreator channelCreator) {
 
+		final RelayConnectionFuture connectionFuture = new RelayConnectionFuture();
+		
 		final Message message = createMessage(other, RELAY_COMMAND, Type.REQUEST_1);
 		FutureResponse futureResponse = new FutureResponse(message);
 		final RequestHandler<FutureResponse> requestHandler = new RequestHandler<FutureResponse>(futureResponse, peerBean(), connectionBean(), config);
-		LOG.debug("send RPC message {}", message);
+		logger.debug("send RPC message {}", message);
 		requestHandler.sendTCP(channelCreator);
-
-		return futureResponse;
+		
+		futureResponse.addListener(new BaseFutureListener<FutureResponse>() {
+			@Override
+			public void operationComplete(FutureResponse future) throws Exception {
+				// TODO Auto-generated method stub
+			}
+			@Override
+			public void exceptionCaught(Throwable t) throws Exception {
+				logger.error("Error creating connection to relay peer {}: {}", other, t);
+				connectionFuture.setFailed(t);
+			}
+		});
+		
+		return connectionFuture;
 
 	}
 
 	@Override
-	public Message handleResponse(Message message, PeerConnection peerConnection, boolean sign) throws Exception {
+    public void handleResponse(final Message message, PeerConnection peerConnection, final boolean sign, Responder responder) throws Exception {
 		if (!(message.getType() == Type.REQUEST_1 && message.getCommand() == RELAY_COMMAND)) {
 			throw new IllegalArgumentException("Message content is wrong");
 		}
 		
-		LOG.debug("received RPC message {}", message);
+		logger.debug("received RPC message {}", message);
 		
 		if(peerBean().serverPeerAddress().isRelay()) {
 			//peer is behind a NAT as well -> deny request
-			return createResponseMessage(message, Type.DENIED);
+			responder.response(createResponseMessage(message, Type.DENIED));
 		} else {
 			PermanentConnectionRPC permanentConnection = new PermanentConnectionRPC(peer, message.getSender());
 			peer.setDirectDataRPC(permanentConnection);
-			return createResponseMessage(message, Type.OK);
+			responder.response(createResponseMessage(message, Type.OK));
 		}
 	}
 }

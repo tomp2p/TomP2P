d42492c407a5e5671ac3fc0158cd42284558cb4d
Raphael Voellmy <r.voellmy@gmail.com>
2013-12-06 11:37:20 +0100

    Added Relay RPC test

--- nat/src/test/java/net/tomp2p/relay/TestRelay.java
+++ nat/src/test/java/net/tomp2p/relay/TestRelay.java
@@ -7,11 +7,10 @@ import java.util.Random;
 import net.tomp2p.Utils2;
 import net.tomp2p.futures.BaseFuture;
 import net.tomp2p.futures.BaseFutureListener;
-import net.tomp2p.futures.Cancel;
 import net.tomp2p.futures.FutureBootstrap;
 import net.tomp2p.futures.FutureChannelCreator;
 import net.tomp2p.futures.FutureDirect;
-import net.tomp2p.futures.BaseFuture.FutureType;
+import net.tomp2p.futures.FuturePeerConnection;
 import net.tomp2p.p2p.Peer;
 import net.tomp2p.p2p.PeerMaker;
 import net.tomp2p.peers.Number160;
@@ -19,18 +18,9 @@ import net.tomp2p.peers.PeerAddress;
 import net.tomp2p.rpc.ObjectDataReply;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class TestRelay {
-
-	Peer[] peers;
-	RelayManager relayManager;
-
-	@Before
-	public void setup() {
-
-	}
 
 	@Test
 	public void testSetupRelayPeers() throws Exception {
@@ -111,21 +101,26 @@ public class TestRelay {
 			rf.awaitUninterruptibly();
 
 			Assert.assertTrue(rf.isSuccess());
+			
+			//Check if flags are set correctly
+			//Assert.assertTrue(slave.getPeerAddress().isRelay()); //TODO
+			Assert.assertFalse(slave.getPeerAddress().isFirewalledTCP());
+			Assert.assertFalse(slave.getPeerAddress().isFirewalledUDP());
 
 			// Ping the unreachable peer from any peer
-//			System.out.print("Send UDP ping to unreachable peer");
-//			BaseFuture f2 = peers[rnd.nextInt(nrOfNodes)].ping().setPeerAddress(slave.getPeerAddress()).start();
-//			f2.awaitUninterruptibly();
-//			Assert.assertTrue(f2.isSuccess());
-//			System.out.println("...done");
-//
-//			System.out.print("Send TCP ping to unreachable peer");
-//			BaseFuture f3 = peers[rnd.nextInt(nrOfNodes)].ping().setTcpPing().setPeerAddress(slave.getPeerAddress()).start();
-//			f3.awaitUninterruptibly();
-//			Assert.assertTrue(f3.isSuccess());
-//			System.out.println("...done");
+			System.out.print("Send UDP ping to unreachable peer");
+			BaseFuture f2 = peers[rnd.nextInt(nrOfNodes)].ping().setPeerAddress(slave.getPeerAddress()).start();
+			f2.awaitUninterruptibly();
+			Assert.assertTrue(f2.isSuccess());
+			System.out.println("...done");
 
-			System.out.println("Send direct message to unreachable peer");
+			System.out.print("Send TCP ping to unreachable peer");
+			BaseFuture f3 = peers[rnd.nextInt(nrOfNodes)].ping().setTcpPing().setPeerAddress(slave.getPeerAddress()).start();
+			f3.awaitUninterruptibly();
+			Assert.assertTrue(f3.isSuccess());
+			System.out.println("...done");
+
+			System.out.print("Send direct message to unreachable peer");
 			final String request = "Hello ";
 			final String response = "World!";
 			slave.setObjectDataReply(new ObjectDataReply() {
@@ -141,45 +136,53 @@ public class TestRelay {
 				public void operationComplete(FutureDirect future) throws Exception {
 					Assert.assertEquals(response, future.object());
 				}
+
 				@Override
 				public void exceptionCaught(Throwable t) throws Exception {
 					Assert.fail(t.getMessage());
 				}
 			});
 			fd.awaitUninterruptibly();
+			System.out.println("...done");
 			Thread.sleep(100);
 
 		} finally {
 			if (master != null) {
 				master.shutdown().await();
 			}
-			
+
 		}
 	}
 
 	@Test
 	public void testRelayRPC() throws Exception {
-		final Random rnd = new Random(42);
-		Peer[] peers = Utils2.createNodes(2, rnd, 4000);
-		Peer master = peers[0]; // the relay peer
-		Peer slave = peers[1];
+		Peer master = null;
+		Peer slave = null;
+		try {
+			final Random rnd = new Random(42);
+			Peer[] peers = Utils2.createNodes(2, rnd, 4000);
+			master = peers[0]; // the relay peer
+			new RelayRPC(master); // register relayRPC ioHandler
+			slave = peers[1];
 
-		// create channel creator
-		FutureChannelCreator fcc = slave.getConnectionBean().reservation().create(1, PeerAddress.MAX_RELAYS);
-		fcc.awaitUninterruptibly();
+			// create channel creator
+			FutureChannelCreator fcc = slave.getConnectionBean().reservation().create(1, PeerAddress.MAX_RELAYS);
+			fcc.awaitUninterruptibly();
 
-		/*
-		 * RelayConnectionFuture rcf = new
-		 * RelayRPC(slave).setupRelay(master.getPeerAddress(),
-		 * fcc.getChannelCreator()); rcf.awaitUninterruptibly();
-		 * System.err.println(rcf.getFailedReason());
-		 * Assert.assertTrue(rcf.isSuccess()); FuturePeerConnection fpc =
-		 * rcf.futurePeerConnection(); System.err.println(fpc);
-		 * Assert.assertTrue
-		 * (fpc.getObject().channelFuture().channel().isActive());
-		 * Assert.assertTrue
-		 * (fpc.getObject().channelFuture().channel().isOpen());
-		 */
+			RelayConnectionFuture rcf = new RelayRPC(slave).setupRelay(master.getPeerAddress(), fcc.getChannelCreator());
+			rcf.awaitUninterruptibly();
+			
+			//Check if permanent peer connection was created
+			Assert.assertTrue(rcf.isSuccess());
+			FuturePeerConnection fpc = rcf.futurePeerConnection();
+			Assert.assertEquals(master.getPeerAddress(), fpc.getObject().remotePeer());
+			Assert.assertTrue(fpc.getObject().channelFuture().channel().isActive());
+			Assert.assertTrue(fpc.getObject().channelFuture().channel().isOpen());
+
+		} finally {
+			master.shutdown().await();
+			slave.shutdown().await();
+		}
 	}
 
 }

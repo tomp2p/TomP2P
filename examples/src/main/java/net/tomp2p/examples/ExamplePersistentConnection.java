c48d2cf5555d27bdb5787c0075dc64143d55fbf0
Thomas Bocek <tom@tomp2p.net>
2014-02-26 10:50:21 +0100

    fixed persistent connection example

--- examples/src/main/java/net/tomp2p/examples/ExamplePersistentConnection.java
+++ examples/src/main/java/net/tomp2p/examples/ExamplePersistentConnection.java
@@ -15,15 +15,25 @@
  */
 package net.tomp2p.examples;
 
+import io.netty.channel.ChannelHandler;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Random;
 
-import net.tomp2p.connection.PeerConnection;
-import net.tomp2p.futures.FutureResponse;
+import net.tomp2p.connection.ChannelClientConfiguration;
+import net.tomp2p.connection.ChannelServerConficuration;
+import net.tomp2p.connection.PipelineFilter;
+import net.tomp2p.futures.FutureDirect;
+import net.tomp2p.futures.FuturePeerConnection;
+import net.tomp2p.message.CountConnectionOutboundHandler;
 import net.tomp2p.p2p.Peer;
 import net.tomp2p.p2p.PeerMaker;
 import net.tomp2p.peers.Number160;
 import net.tomp2p.peers.PeerAddress;
 import net.tomp2p.rpc.ObjectDataReply;
+import net.tomp2p.utils.Pair;
 
 /**
  * Example how to use persistent connection with Peer.sendDirect().
@@ -40,6 +50,8 @@ public final class ExamplePersistentConnection {
     }
 
     private static final Random RND = new Random(42L);
+    private static final CountConnectionOutboundHandler ccohTCP = new CountConnectionOutboundHandler();
+    private static final CountConnectionOutboundHandler ccohUDP = new CountConnectionOutboundHandler();
 
     /**
      * Start the example with persistent connections.
@@ -65,7 +77,13 @@ public final class ExamplePersistentConnection {
             final int port1 = 4001;
             final int port2 = 4002;
             final int timeout = 20;
-            peer1 = new PeerMaker(new Number160(RND)).ports(port1).makeAndListen();
+            
+            ChannelServerConficuration csc = PeerMaker.createDefaultChannelServerConfiguration();
+    		ChannelClientConfiguration ccc = PeerMaker.createDefaultChannelClientConfiguration();
+    		csc.pipelineFilter(createFilter());
+    		ccc.pipelineFilter(createFilter());
+            
+            peer1 = new PeerMaker(new Number160(RND)).ports(port1).channelClientConfiguration(ccc).channelServerConfiguration(csc).makeAndListen();
             peer2 = new PeerMaker(new Number160(RND)).ports(port2).makeAndListen();
             //
             peer2.setObjectDataReply(new ObjectDataReply() {
@@ -74,22 +92,31 @@ public final class ExamplePersistentConnection {
                     return "world!";
                 }
             });
-            // keep the connection for 20s alive. Setting -1 means to keep it open as long as possible
-            PeerConnection peerConnection = peer1.createPeerConnection(peer2.getPeerAddress(), timeout);
+            
             String sentObject = "Hello";
-            FutureResponse fd = peer1.sendDirect(peerConnection).setObject(sentObject).start();
+            FutureDirect fd = peer1.sendDirect(peer2.getPeerAddress()).setObject(sentObject).start();
             System.out.println("send " + sentObject);
             fd.awaitUninterruptibly();
-            System.out.println("received " + fd.getObject() + " connections: "
-                    + peer1.getPeerBean().getStatistics().getTCPChannelCreationCount());
+            
+            System.out.println("received " + fd.object() + " connections: "
+                    + ccohTCP.total()+ "/"+ccohUDP.total());
+            
+            // keep the connection for 20s alive. Setting -1 means to keep it open as long as possible
+            FuturePeerConnection futurePeerConnection = peer1.createPeerConnection(peer2.getPeerAddress(), timeout);
+            
+            fd = peer1.sendDirect(futurePeerConnection).setObject(sentObject).start();
+            System.out.println("send " + sentObject);
+            fd.awaitUninterruptibly();
+            System.out.println("received " + fd.object() + " connections: "
+                    + ccohTCP.total()+ "/"+ccohUDP.total());
             // we reuse the connection
-            fd = peer1.sendDirect(peerConnection).setObject(sentObject).start();
+            fd = peer1.sendDirect(futurePeerConnection).setObject(sentObject).start();
             System.out.println("send " + sentObject);
             fd.awaitUninterruptibly();
-            System.out.println("received " + fd.getObject() + " connections: "
-                    + peer1.getPeerBean().getStatistics().getTCPChannelCreationCount());
+            System.out.println("received " + fd.object() + " connections: "
+                    + ccohTCP.total()+ "/"+ccohUDP.total());
             // now we don't want to keep the connection open anymore:
-            peerConnection.close();
+            futurePeerConnection.close();
         } finally {
             if (peer1 != null) {
                 peer1.shutdown();
@@ -98,6 +125,21 @@ public final class ExamplePersistentConnection {
                 peer2.shutdown();
             }
         }
+    }
+    
+    private static PipelineFilter createFilter() {
+    	
+    	PipelineFilter pf = new PipelineFilter() {
+			@Override
+			public Map<String, Pair<EventExecutorGroup, ChannelHandler>> filter(Map<String, Pair<EventExecutorGroup, ChannelHandler>> channelHandlers, boolean tcp,
+			        boolean client) {
+				Map<String, Pair<EventExecutorGroup, ChannelHandler>> retVal = new LinkedHashMap<String, Pair<EventExecutorGroup, ChannelHandler>>();
+				retVal.put("counter", new Pair<EventExecutorGroup, ChannelHandler>(null, tcp? ccohTCP:ccohUDP));
+				retVal.putAll(channelHandlers);
+				return retVal;
+			}
+		};
+		return pf;
     }
 
 }

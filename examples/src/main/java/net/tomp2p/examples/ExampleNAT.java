18c1f2c302f35872f68a0b1a5c57092b3d109f7c
Thomas Bocek <tom@tomp2p.net>
2014-02-26 10:25:23 +0100

    fixed nat examples

--- examples/src/main/java/net/tomp2p/examples/ExampleNAT.java
+++ examples/src/main/java/net/tomp2p/examples/ExampleNAT.java
@@ -19,88 +19,66 @@ import java.net.InetAddress;
 import java.util.Random;
 
 import net.tomp2p.connection.Bindings;
-import net.tomp2p.connection.ChannelCreator;
-import net.tomp2p.futures.FutureChannelCreator;
+import net.tomp2p.futures.BaseFuture;
 import net.tomp2p.futures.FutureDiscover;
-import net.tomp2p.futures.FutureResponse;
 import net.tomp2p.p2p.Peer;
 import net.tomp2p.p2p.PeerMaker;
 import net.tomp2p.peers.Number160;
 import net.tomp2p.peers.PeerAddress;
 
-public class ExampleNAT
-{
-    public void startServer()
-        throws Exception
-    {
-        Peer peer = null;
-        try
-        {
-            Random r = new Random( 42L );
-            // peer.getP2PConfiguration().setBehindFirewall(true);
-            Bindings b = new Bindings().addInterface( "eth0" );
-            peer = new PeerMaker( new Number160( r ) ).bindings( b ).ports( 4000 ).makeAndListen();
-            System.out.println( "peer started." );
-            for ( ;; )
-            {
-                for ( PeerAddress pa : peer.getPeerBean().peerMap().getAll() )
-                {
-                    FutureChannelCreator fcc = peer.getConnectionBean().getConnectionReservation().reserve( 1 );
-                    fcc.awaitUninterruptibly();
-                    ChannelCreator cc = fcc.getChannelCreator();
-                    FutureResponse fr1 = peer.getHandshakeRPC().pingTCP( pa, cc );
-                    fr1.awaitUninterruptibly();
-                    if ( fr1.isSuccess() )
-                        System.out.println( "peer online T:" + pa );
-                    else
-                        System.out.println( "offline " + pa );
-                    FutureResponse fr2 = peer.getHandshakeRPC().pingUDP( pa, cc );
-                    fr2.awaitUninterruptibly();
-                    peer.getConnectionBean().getConnectionReservation().release( cc );
-                    if ( fr2.isSuccess() )
-                        System.out.println( "peer online U:" + pa );
-                    else
-                        System.out.println( "offline " + pa );
-                }
-                Thread.sleep( 1500 );
-            }
-        }
-        finally
-        {
-            peer.shutdown();
-        }
-    }
+public class ExampleNAT {
+	public void startServer() throws Exception {
+		Peer peer = null;
+		try {
+			Random r = new Random(42L);
+			// peer.getP2PConfiguration().setBehindFirewall(true);
+			Bindings b = new Bindings().addInterface("eth0");
+			peer = new PeerMaker(new Number160(r)).bindings(b).ports(4000).makeAndListen();
+			System.out.println("peer started.");
+			for (;;) {
+				for (PeerAddress pa : peer.getPeerBean().peerMap().getAll()) {
+					BaseFuture future = peer.ping().setPeerAddress(pa).setTcpPing().start();
+					if (future.isSuccess()) {
+						System.out.println("peer online (TCP):" + pa);
+					}
+					else {
+						System.out.println("offline " + pa);
+					}
+					future = peer.ping().setPeerAddress(pa).start();
+					if (future.isSuccess()) {
+						System.out.println("peer online (UDP):" + pa);
+					}
+					else {
+						System.out.println("offline " + pa);
+					}
+				}
+				Thread.sleep(1500);
+			}
+		} finally {
+			peer.shutdown();
+		}
+	}
 
-    public static void main( String[] args )
-        throws Exception
-    {
-        if ( args.length > 0 )
-        {
-            startClientNAT( args[0] );
-        }
-        else
-        {
-            ExampleNAT t = new ExampleNAT();
-            t.startServer();
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		if (args.length > 0) {
+			startClientNAT(args[0]);
+		} else {
+			ExampleNAT t = new ExampleNAT();
+			t.startServer();
+		}
+	}
 
-    public static void startClientNAT( String ip )
-        throws Exception
-    {
-        Random r = new Random( 43L );
-        Peer peer = new PeerMaker( new Number160( r ) ).ports( 4000 ).setBehindFirewall().makeAndListen();
-        PeerAddress pa = new PeerAddress( Number160.ZERO, InetAddress.getByName( ip ), 4000, 4000 );
-        FutureDiscover fd = peer.discover().peerAddress( pa ).start();
-        fd.awaitUninterruptibly();
-        if ( fd.isSuccess() )
-        {
-            System.out.println( "found that my outside address is " + fd.getPeerAddress() );
-        }
-        else
-        {
-            System.out.println( "failed " + fd.getFailedReason() );
-        }
-        peer.shutdown();
-    }
+	public static void startClientNAT(String ip) throws Exception {
+		Random r = new Random(43L);
+		Peer peer = new PeerMaker(new Number160(r)).ports(4000).setBehindFirewall().makeAndListen();
+		PeerAddress pa = new PeerAddress(Number160.ZERO, InetAddress.getByName(ip), 4000, 4000);
+		FutureDiscover fd = peer.discover().peerAddress(pa).start();
+		fd.awaitUninterruptibly();
+		if (fd.isSuccess()) {
+			System.out.println("found that my outside address is " + fd.getPeerAddress());
+		} else {
+			System.out.println("failed " + fd.getFailedReason());
+		}
+		peer.shutdown();
+	}
 }

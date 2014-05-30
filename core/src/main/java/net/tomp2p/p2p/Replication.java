5ce5e9ee35a328c9e79115746003f2325cdb14b1
Ippes <seppi.gol@gmail.com>
2014-05-30 12:12:12 +0200

    small format correction

--- core/src/main/java/net/tomp2p/p2p/Replication.java
+++ core/src/main/java/net/tomp2p/p2p/Replication.java
@@ -8,7 +8,7 @@
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHerr
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
@@ -32,7 +32,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This class has 3 methods that are called from errside eventes: check, peerInsert, peerRemoved.
+ * This class has 3 methods that are called from outside eventes: check, peerInsert, peerRemoved.
  */
 public class Replication implements PeerMapChangeListener {
     private static final Logger LOG = LoggerFactory.getLogger(Replication.class);

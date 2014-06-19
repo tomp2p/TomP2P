0f346e4baa84a2f32b7a8a19c93165c1ac4662be
Jonas Wagner <jonas_wagner99@hotmail.com>
2014-06-19 15:14:14 +0200

    driver fix

--- nat/src/main/java/net/tomp2p/rcon/RconDriver.java
+++ nat/src/main/java/net/tomp2p/rcon/RconDriver.java
@@ -3,30 +3,32 @@ package net.tomp2p.rcon;
 public class RconDriver {
 
 	public static void main(String[] args) {
+		boolean success = false;
+		
 		SimpleRconClient.start();
 		if (args.length == 1) {
-			boolean success = SimpleRconClient.usualBootstrap(args[0]);
+			success = SimpleRconClient.usualBootstrap(args[0]);
 			System.out.println();
 			System.out.println("usualBootstrap Success: " + success);
 			System.out.println();
-			if (!success) {
-				System.out.println();
-				System.out.println("Start relaying");
-				System.out.println();
-
-				SimpleRconClient.natBootstrap(args[0]);
-
-				System.out.println();
-				System.out.println("SENDDUMMY");
-				System.out.println();
-
-				SimpleRconClient.sendDummy("yoMama", true);
-			} else {
-				System.out.println();
-				System.out.println("SENDDUMMY");
-				System.out.println();
-				SimpleRconClient.sendDummy("yoMama", false);
-			}
+		}
+		if (!success) {
+			System.out.println();
+			System.out.println("Start relaying");
+			System.out.println();
+			
+			SimpleRconClient.natBootstrap(args[0]);
+			
+			System.out.println();
+			System.out.println("SENDDUMMY");
+			System.out.println();
+			
+			SimpleRconClient.sendDummy("yoMama", true);
+		} else {
+			System.out.println();
+			System.out.println("SENDDUMMY");
+			System.out.println();
+			SimpleRconClient.sendDummy("yoMama", false);
 		}
 	}
 }

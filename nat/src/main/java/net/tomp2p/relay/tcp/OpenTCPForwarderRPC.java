f94c71ba5b275d0fb686830f191b0dcba6214afd
nicoruti <nico.rutishauser@diligent-it.com>
2014-10-09 10:21:31 +0200

    Fix NPE and simplify the message response creation on TCP Relay

--- nat/src/main/java/net/tomp2p/relay/tcp/OpenTCPForwarderRPC.java
+++ nat/src/main/java/net/tomp2p/relay/tcp/OpenTCPForwarderRPC.java
@@ -1,5 +1,7 @@
 package net.tomp2p.relay.tcp;
 
+import java.net.InetSocketAddress;
+
 import net.tomp2p.connection.ConnectionConfiguration;
 import net.tomp2p.connection.PeerConnection;
 import net.tomp2p.connection.PeerException;
@@ -50,39 +52,41 @@ public class OpenTCPForwarderRPC extends BaseRelayForwarderRPC {
 		super(peer, peerConnection, RelayType.OPENTCP);
 		this.config = config;
 		this.peerConnection = peerConnection.changeRemotePeer(unreachablePeerAddress());
-		
+
 		// add a listener when the connection is closed
 		peerConnection.closeFuture().addListener(new BaseFutureAdapter<FutureDone<Void>>() {
 			@Override
-            public void operationComplete(FutureDone<Void> future) throws Exception {
+			public void operationComplete(FutureDone<Void> future) throws Exception {
 				peer.peerBean().removePeerStatusListeners(OpenTCPForwarderRPC.this);
 				peer.connectionBean().dispatcher().removeIoHandler(peer.peerID(), unreachablePeerId());
-            }
+			}
 		});
-		
+
 		LOG.debug("Created TCP forwarder from peer {} to peer {}", peer.peerAddress(), unreachablePeerAddress());
 	}
-	
-	@Override
-    public boolean peerFailed(PeerAddress remotePeer, PeerException exception) {
-	    //not handled here
-	    return false;
-    }
 
 	@Override
-    public boolean peerFound(PeerAddress remotePeer, PeerAddress referrer, PeerConnection peerConnection2) {
+	public boolean peerFailed(PeerAddress remotePeer, PeerException exception) {
+		// not handled here
+		return false;
+	}
+
+	@Override
+	public boolean peerFound(PeerAddress remotePeer, PeerAddress referrer, PeerConnection peerConnection2) {
 		boolean firstHand = referrer == null;
 		boolean secondHand = remotePeer.equals(referrer);
 		boolean samePeerConnection = peerConnection.equals(peerConnection2);
-		//if firsthand, then full trust, if second hand and a stable peerconnection, we can trust as well
-		if((firstHand || (secondHand && samePeerConnection))  && remotePeer.peerId().equals(unreachablePeerId()) && remotePeer.isRelayed()) {
-			//we got new information about this peer, e.g. its active relays
-			LOG.trace("Update the unreachable peer to {} based on {}, ref {}", unreachablePeerAddress(), remotePeer, referrer);
+		// if firsthand, then full trust, if second hand and a stable peerconnection, we can trust as well
+		if ((firstHand || (secondHand && samePeerConnection)) && remotePeer.peerId().equals(unreachablePeerId())
+				&& remotePeer.isRelayed()) {
+			// we got new information about this peer, e.g. its active relays
+			LOG.trace("Update the unreachable peer to {} based on {}, ref {}", unreachablePeerAddress(), remotePeer,
+					referrer);
 			unreachablePeerAddress(remotePeer);
 		}
-	    return false;
-    }
-	
+		return false;
+	}
+
 	@Override
 	public FutureDone<Message> forwardToUnreachable(final Message message) {
 		// Send message via direct message through the open connection to the unreachable peer
@@ -96,38 +100,45 @@ public class OpenTCPForwarderRPC extends BaseRelayForwarderRPC {
 			LOG.error("Cannot encode the message", e);
 			return new FutureDone<Message>().failed(e);
 		}
-		
+
 		// always keep the connection open
 		envelope.keepAlive(true);
 
 		// holds the message that will be returned to he requester
 		final FutureDone<Message> futureDone = new FutureDone<Message>();
-		
-		// Forward a message through the open peer connection to the unreachable  peer.
+
+		// Forward a message through the open peer connection to the unreachable peer.
 		FutureResponse fr = RelayUtils.send(peerConnection, peerBean(), connectionBean(), config, envelope);
 		fr.addListener(new BaseFutureAdapter<FutureResponse>() {
 			public void operationComplete(FutureResponse future) throws Exception {
 				if (future.isSuccess()) {
+					InetSocketAddress senderSocket = message.recipientSocket();
+					if (senderSocket == null) {
+						senderSocket = unreachablePeerAddress().createSocketTCP();
+					}
+					InetSocketAddress recipientSocket = message.senderSocket();
+					if (recipientSocket == null) {
+						recipientSocket = message.sender().createSocketTCP();
+					}
+
 					Buffer buffer = future.responseMessage().buffer(0);
-					Message responseFromUnreachablePeer = RelayUtils.decodeMessage(buffer, message.recipientSocket(),
-					        message.senderSocket());
+					Message responseFromUnreachablePeer = RelayUtils.decodeMessage(buffer, recipientSocket, senderSocket);
 					responseFromUnreachablePeer.restoreContentReferences();
-					responseFromUnreachablePeer.sender(peerConnection.remotePeer());
-					responseFromUnreachablePeer.recipient(message.sender());
 					futureDone.done(responseFromUnreachablePeer);
 				} else {
 					futureDone.failed("Could not forward message over TCP channel");
 				}
 			}
 		});
-		
+
 		return futureDone;
 	}
-	
+
 	@Override
 	protected void handlePing(Message message, Responder responder) {
 		LOG.debug("peerconnection open? {}", peerConnection.isOpen());
-		Message response = createResponseMessage(message, peerConnection.isOpen() ? Type.OK : Type.EXCEPTION, unreachablePeerAddress());
+		Message response = createResponseMessage(message, peerConnection.isOpen() ? Type.OK : Type.EXCEPTION,
+				unreachablePeerAddress());
 		responder.response(response);
 	}
 }

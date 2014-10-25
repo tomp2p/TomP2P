59e5db391016c91c37fde3e30b081943ae44180c
Christian Lüthold <biocodr@gmail.com>
2014-10-25 19:02:55 +0200

    java <-> .net int/long conversion (-,0,+)
    Signed-off-by: Christian Lüthold <biocodr@gmail.com>

--- core/src/test/java/net/tomp2p/message/TestDotNetInterop.java
+++ core/src/test/java/net/tomp2p/message/TestDotNetInterop.java
@@ -9,6 +9,7 @@ import java.io.FileOutputStream;
 
 import net.tomp2p.storage.AlternativeCompositeByteBuf;
 
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -20,24 +21,19 @@ import org.junit.Test;
  */
 public class TestDotNetInterop {
 
-	private final String from = "D:/Desktop/interop/bytes-NET-encoded.txt";
-	private final String to = "D:/Desktop/interop/bytes-JAVA-encoded.txt";
+	//private final String from = "D:/Desktop/interop/bytes-NET-encoded.txt";
+	//private final String to = "D:/Desktop/interop/bytes-JAVA-encoded.txt";
+	private final String from = "C:/Users/Christian/Desktop/interop/bytes-NET-encoded.txt";
+	private final String to = "C:/Users/Christian/Desktop/interop/bytes-JAVA-encoded.txt";
 	
 	@Ignore
 	@Test
-	public void testEncode()throws Exception {
-		
-		//Message m1 = Utils2.createDummyMessage();
-		//final int integer = 42;
-		//m1.intValue(integer);
-		
-		//Encoder encoder = new Encoder(null);
+	public void testEncodeInt()throws Exception {
+
 		AlternativeCompositeByteBuf buf = AlternativeCompositeByteBuf.compBuffer();
-		
-		//encoder.write(buf, m1, null);
-		
-		final int value = Integer.MAX_VALUE; // 2147483647
-		buf.writeInt(value);
+		buf.writeInt(Integer.MIN_VALUE); //-2147483648
+		buf.writeInt(0);
+		buf.writeInt(Integer.MAX_VALUE); // 2147483647
 
 		byte[] bytes = buf.array();
 		
@@ -53,7 +49,7 @@ public class TestDotNetInterop {
 	
 	@Ignore
 	@Test
-	public void testDecode() throws Exception {
+	public void testDecodeInt() throws Exception {
 		
 		FileInputStream fis = new FileInputStream(from);
 		byte[] fileContent = new byte[from.length()];
@@ -65,6 +61,84 @@ public class TestDotNetInterop {
 		}
 		
 		ByteBuf buf = Unpooled.copiedBuffer(fileContent);
-		int value = buf.readInt();
+		
+		int minVal = buf.readInt();
+		int zero = buf.readInt();
+		int maxVal = buf.readInt();
+		
+		Assert.assertTrue(minVal == Integer.MIN_VALUE);
+		Assert.assertTrue(zero == 0);
+		Assert.assertTrue(maxVal == Integer.MAX_VALUE);
+		
+	}
+	
+	@Ignore
+	@Test
+	public void testEncodeLong() throws Exception {
+		
+		AlternativeCompositeByteBuf buf = AlternativeCompositeByteBuf.compBuffer();
+		
+		buf.writeLong(Long.MIN_VALUE); //-923372036854775808
+		buf.writeLong(0);
+		buf.writeLong(Long.MAX_VALUE); // 923372036854775807
+
+		byte[] bytes = buf.array();
+		
+		File file = new File(to);
+		FileOutputStream fos = new FileOutputStream(file);
+		try {
+			fos.write(bytes);
+		}
+		finally {
+			fos.close();
+		}
+	}
+	
+	@Ignore
+	@Test
+	public void testDecodeLong() throws Exception {
+		
+		FileInputStream fis = new FileInputStream(from);
+		byte[] fileContent = new byte[from.length()];
+		try {
+			fis.read(fileContent);
+		}
+		finally {
+			fis.close();
+		}
+		
+		ByteBuf buf = Unpooled.copiedBuffer(fileContent);
+		
+		long minVal = buf.readLong();
+		long zero = buf.readLong();
+		long maxVal = buf.readLong();
+		
+		Assert.assertTrue(minVal == Long.MIN_VALUE);
+		Assert.assertTrue(zero == 0);
+		Assert.assertTrue(maxVal == Long.MAX_VALUE);
+	}
+	
+	@Ignore
+	@Test
+	public void testEncodeByte() {
+		
+	}
+	
+	@Ignore
+	@Test
+	public void testDecodeByte() {
+		
+	}
+	
+	@Ignore
+	@Test
+	public void testEncodeBytes() {
+		
+	}
+	
+	@Ignore
+	@Test
+	public void testDecodeBytes() {
+		
 	}
 }

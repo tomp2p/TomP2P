d077cfd25e3c67b05a8f358764755504847d5dc6
Thomas Bocek <tom@tomp2p.net>
2014-10-29 22:11:42 +0100

    Fix resource leakage in capacity (similar to f2152321f6e21ea7caac903ecde5717c16c3e77d)

--- core/src/main/java/net/tomp2p/storage/AlternativeCompositeByteBuf.java
+++ core/src/main/java/net/tomp2p/storage/AlternativeCompositeByteBuf.java
@@ -253,8 +253,8 @@ public class AlternativeCompositeByteBuf extends ByteBuf {
 				Component c = i.previous();
 				if (bytesToTrim >= c.buf.capacity()) {
 					bytesToTrim -= c.buf.capacity();
-					i.remove();
 					c.buf.release();
+					i.remove();
 					continue;
 				}
 				Component newC = new Component(c.buf.slice(0, c.buf.capacity()

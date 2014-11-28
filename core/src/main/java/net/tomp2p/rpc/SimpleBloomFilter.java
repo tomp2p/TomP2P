54fefb143df355831ed72e2930c3d78d90763b50
Christian Lüthold <biocodr@gmail.com>
2014-11-28 10:27:52 +0100

    use constant
    Signed-off-by: Christian Lüthold <biocodr@gmail.com>

--- core/src/main/java/net/tomp2p/rpc/SimpleBloomFilter.java
+++ core/src/main/java/net/tomp2p/rpc/SimpleBloomFilter.java
@@ -102,7 +102,7 @@ public class SimpleBloomFilter<E> implements Set<E>, Serializable {
 	 *            The byte buffer with the data
 	 */
 	public SimpleBloomFilter(final ByteBuf channelBuffer) {
-		this.byteArraySize = channelBuffer.readUnsignedShort() - (SIZE_HEADER_ELEMENTS + SIZE_HEADER_LENGTH);
+		this.byteArraySize = channelBuffer.readUnsignedShort() - SIZE_HEADER;
 		this.bitArraySize = byteArraySize * Byte.SIZE;
 		int expectedElements = channelBuffer.readInt();
 		this.expectedElements = expectedElements;

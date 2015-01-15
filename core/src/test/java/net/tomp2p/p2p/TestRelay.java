07496e111d8b885ad95240c67216f1348d4b916b
tbocek <tom@tomp2p.net>
2015-01-15 19:46:02 +0100

    increased logging to hunt down bug when running with travis

--- core/src/test/java/net/tomp2p/p2p/TestRelay.java
+++ core/src/test/java/net/tomp2p/p2p/TestRelay.java
@@ -18,78 +18,99 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestRelay {
-    private final static Random rnd = new Random(42L);
+	private final static Random rnd = new Random(42L);
+	
+	@Test 
+	public void testLoop() throws Exception {
+		for(int i=0;i<1000;i++) {
+			 testPeerConnection();
+		}
+	}
 
-    @Test
-    public void testPeerConnection() throws Exception {
-        Peer master = null;
-        Peer slave = null;
-        try {
-        master = Utils2.createNodes(1, rnd, 4001, null, false)[0];
-        slave = Utils2.createNodes(1, rnd, 4002, null, false)[0];
-        System.err.println("master is " + master.peerAddress());
-        System.err.println("slave is " + slave.peerAddress());
-        
-        FuturePeerConnection pcMaster = master.createPeerConnection(slave.peerAddress());
-        MyDirectDataRPC myDirectDataRPC = new MyDirectDataRPC(slave.peerBean(), slave.connectionBean());
-        slave.directDataRPC(myDirectDataRPC);
-        
-        slave.objectDataReply(new ObjectDataReply() {
-            @Override
-            public Object reply(PeerAddress sender, Object request) throws Exception {
-                return "yoo!";
-            }
-        });
-        
-        master.objectDataReply(new ObjectDataReply() {
-            @Override
-            public Object reply(PeerAddress sender, Object request) throws Exception {
-                return "world!";
-            }
-        });
-        
-        FutureDirect futureResponse = master.sendDirect(pcMaster).object("test").start().awaitUninterruptibly();
-        Assert.assertEquals("yoo!", futureResponse.object());
-        
-        FuturePeerConnection pcSlave = myDirectDataRPC.peerConnection();
-        
-        futureResponse = slave.sendDirect(pcSlave).object("hello").start().awaitUninterruptibly();
-        System.err.println(futureResponse.failedReason());
-        Assert.assertEquals("world!", futureResponse.object());
-        
-        Thread.sleep(1000);
-        pcSlave.close();
-        pcMaster.close();
-        System.err.println("done");
-        
-        } finally {
-            if (master != null) {
-                master.shutdown().await();
-            }
-            if (slave != null) {
-                slave.shutdown().await();
-            }
-        }
+	@Test
+	public void testPeerConnection() throws Exception {
+		Peer master = null;
+		Peer slave = null;
+		try {
+			master = Utils2.createNodes(1, rnd, 4001, null, false)[0];
+			slave = Utils2.createNodes(1, rnd, 4002, null, false)[0];
+			System.err.println("master is " + master.peerAddress());
+			System.err.println("slave is " + slave.peerAddress());
 
-    }
-    
-    private static class MyDirectDataRPC extends DirectDataRPC {
-        
-        private FuturePeerConnection futurePeerConnection;
+			FuturePeerConnection pcMaster = master.createPeerConnection(slave
+					.peerAddress());
+			MyDirectDataRPC myDirectDataRPC = new MyDirectDataRPC(
+					slave.peerBean(), slave.connectionBean());
+			slave.directDataRPC(myDirectDataRPC);
 
-        MyDirectDataRPC(PeerBean peerBean, ConnectionBean connectionBean) {
-            super(peerBean, connectionBean);
-        }
-        @Override
-        public void handleResponse(Message message, PeerConnection peerConnection, boolean sign, Responder responder)
-                throws Exception {
-            futurePeerConnection = new FuturePeerConnection(message.sender());
-            futurePeerConnection.done(peerConnection);
-            super.handleResponse(message, peerConnection, sign, responder);
-        }
-        
-        public FuturePeerConnection peerConnection() {
-            return futurePeerConnection;
-        }
-    }
+			slave.objectDataReply(new ObjectDataReply() {
+				@Override
+				public Object reply(PeerAddress sender, Object request)
+						throws Exception {
+					return "yoo!";
+				}
+			});
+
+			master.objectDataReply(new ObjectDataReply() {
+				@Override
+				public Object reply(PeerAddress sender, Object request)
+						throws Exception {
+					return "world!";
+				}
+			});
+
+			FutureDirect futureResponse = master.sendDirect(pcMaster)
+					.object("test").start().awaitUninterruptibly();
+			System.err.println("testPeerConnection: "+futureResponse);
+			System.err.println("testPeerConnection: "+futureResponse.failedReason());
+			System.err.println("testPeerConnection: "+futureResponse.futureResponse());
+			if(futureResponse.futureResponse() != null) {
+				System.err.println("testPeerConnection: "+futureResponse.futureResponse().responseMessage());
+			}
+			Assert.assertEquals("yoo!", futureResponse.object());
+
+			FuturePeerConnection pcSlave = myDirectDataRPC.peerConnection();
+
+			futureResponse = slave.sendDirect(pcSlave).object("hello").start()
+					.awaitUninterruptibly();
+			System.err.println(futureResponse.failedReason());
+			Assert.assertEquals("world!", futureResponse.object());
+
+			//Thread.sleep(1000);
+			pcSlave.close();
+			pcMaster.close();
+			System.err.println("done");
+
+		} finally {
+			if (master != null) {
+				master.shutdown().await();
+			}
+			if (slave != null) {
+				slave.shutdown().await();
+			}
+		}
+
+	}
+
+	private static class MyDirectDataRPC extends DirectDataRPC {
+
+		private FuturePeerConnection futurePeerConnection;
+
+		MyDirectDataRPC(PeerBean peerBean, ConnectionBean connectionBean) {
+			super(peerBean, connectionBean);
+		}
+
+		@Override
+		public void handleResponse(Message message,
+				PeerConnection peerConnection, boolean sign, Responder responder)
+				throws Exception {
+			futurePeerConnection = new FuturePeerConnection(message.sender());
+			futurePeerConnection.done(peerConnection);
+			super.handleResponse(message, peerConnection, sign, responder);
+		}
+
+		public FuturePeerConnection peerConnection() {
+			return futurePeerConnection;
+		}
+	}
 }

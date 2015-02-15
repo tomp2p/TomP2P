8b5d30c0baaa917fd702e8840f574c938f376ed0
nicoruti <nico.rutishauser@diligent-it.com>
2015-02-15 19:25:32 +0100

    Add version fork test

--- dht/src/test/java/net/tomp2p/dht/TestH2H.java
+++ dht/src/test/java/net/tomp2p/dht/TestH2H.java
@@ -1,5 +1,8 @@
 package net.tomp2p.dht;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 import java.io.Serializable;
 import java.security.InvalidKeyException;
@@ -11,11 +14,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import net.tomp2p.connection.DSASignatureFactory;
+import net.tomp2p.dht.StorageLayer.PutStatus;
 import net.tomp2p.p2p.PeerBuilder;
 import net.tomp2p.peers.Number160;
 import net.tomp2p.peers.Number640;
+import net.tomp2p.peers.PeerAddress;
 import net.tomp2p.storage.Data;
 import net.tomp2p.utils.Utils;
 
@@ -431,6 +437,60 @@ public class TestH2H {
 				p2.shutdown().awaitUninterruptibly();
 			}
 		}
+	}
+	
+	@Test
+	public void testVersionFork() throws Exception {
+		KeyPairGenerator gen = KeyPairGenerator.getInstance("DSA");
+		KeyPair keyPair1 = gen.generateKeyPair();
+
+		PeerDHT p1 = new PeerBuilderDHT(new PeerBuilder(Number160.createHash(1)).ports(4838).start()).start();
+		PeerDHT p2 = new PeerBuilderDHT(new PeerBuilder(Number160.createHash(2)).masterPeer(p1.peer()).start()).start();
+
+		p2.peer().bootstrap().peerAddress(p1.peerAddress()).start().awaitUninterruptibly();
+
+		Number160 locationKey = Number160.createHash(randomString(1));
+		Number160 contentKey = Number160.createHash(randomString(2));
+
+		Data versionA = new Data("versionA").addBasedOn(Number160.ZERO).protectEntry(keyPair1);
+		Data versionB = new Data("versionB").addBasedOn(Number160.ZERO).protectEntry(keyPair1);
+
+		FuturePut putA = p1.put(locationKey).data(contentKey, versionA, Number160.ONE).keyPair(keyPair1).start()
+				.awaitUninterruptibly();
+		assertTrue(putA.isSuccess());
+		assertFalse(hasVersionFork(putA));
+
+		// put version B where a version conflict should be detected because it is not based on version A
+		FuturePut putB = p1.put(locationKey).data(contentKey, versionB, Number160.ONE).keyPair(keyPair1).start()
+				.awaitUninterruptibly();
+		assertTrue(hasVersionFork(putB));
+	}
+
+	private static boolean hasVersionFork(FuturePut future) throws Exception {
+		if (future.isFailed() || future.rawResult().isEmpty()) {
+			throw new Exception("Future failed");
+		}
+
+		for (PeerAddress peeradress : future.rawResult().keySet()) {
+			Map<Number640, Byte> map = future.rawResult().get(peeradress);
+			if (map != null) {
+				for (Number640 key : map.keySet()) {
+					byte putStatus = map.get(key);
+					if (putStatus == -1) {
+						throw new Exception("Got an invalid status: " + putStatus);
+					} else {
+						switch (PutStatus.values()[putStatus]) {
+							case VERSION_FORK:
+								return true;
+							default:
+								break;
+						}
+					}
+				}
+			}
+		}
+
+		return false;
 	}
 
 	private String randomString(int i) {

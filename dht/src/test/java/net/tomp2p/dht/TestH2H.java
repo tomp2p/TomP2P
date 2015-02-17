18e6520bd69d14f5e4e4f8f45752cf238f1b530d
Thomas Bocek <tom@tomp2p.net>
2015-02-17 12:03:14 +0100

    fixed testing, did not shutdown peers

--- dht/src/test/java/net/tomp2p/dht/TestH2H.java
+++ dht/src/test/java/net/tomp2p/dht/TestH2H.java
@@ -438,35 +438,39 @@ public class TestH2H {
 	
 	@Test
 	public void testVersionFork() throws Exception {
-		KeyPairGenerator gen = KeyPairGenerator.getInstance("DSA");
-		KeyPair keyPair1 = gen.generateKeyPair();
-		PeerDHT p1 = new PeerBuilderDHT(
-				new PeerBuilder(Number160.createHash(1)).ports(4838).start())
-				.start();
-		PeerDHT p2 = new PeerBuilderDHT(
-				new PeerBuilder(Number160.createHash(2)).masterPeer(p1.peer())
-						.start()).start();
-		p2.peer().bootstrap().peerAddress(p1.peerAddress()).start()
-				.awaitUninterruptibly();
-		p1.peer().bootstrap().peerAddress(p2.peerAddress()).start()
-			.awaitUninterruptibly();
-		Number160 locationKey = Number160.createHash(randomString(1));
-		Number160 contentKey = Number160.createHash(randomString(2));
-		Data versionA = new Data("versionA").addBasedOn(new Number160(0, Number160.ZERO))
-				.protectEntry(keyPair1);
-		Data versionB = new Data("versionB").addBasedOn(new Number160(0, Number160.ONE))
-				.protectEntry(keyPair1);
-		FuturePut putA = p1.put(locationKey)
-				.data(contentKey, versionA, Number160.ONE).keyPair(keyPair1)
-				.start().awaitUninterruptibly();
-		Assert.assertTrue(putA.isSuccess());
-		Assert.assertFalse(hasVersionFork(putA));
-		// put version B where a version conflict should be detected because it
-		// is not based on version A
-		FuturePut putB = p1.put(locationKey)
-				.data(contentKey, versionB, Number160.ONE).keyPair(keyPair1)
-				.start().awaitUninterruptibly();
-		Assert.assertTrue(hasVersionFork(putB));
+
+		PeerDHT p1 = null;
+		PeerDHT p2 = null;
+		try {
+
+			KeyPairGenerator gen = KeyPairGenerator.getInstance("DSA");
+			KeyPair keyPair1 = gen.generateKeyPair();
+			p1 = new PeerBuilderDHT(new PeerBuilder(Number160.createHash(1)).ports(4838).start()).start();
+			p2 = new PeerBuilderDHT(new PeerBuilder(Number160.createHash(2)).masterPeer(p1.peer()).start()).start();
+			p2.peer().bootstrap().peerAddress(p1.peerAddress()).start().awaitUninterruptibly();
+			p1.peer().bootstrap().peerAddress(p2.peerAddress()).start().awaitUninterruptibly();
+			Number160 locationKey = Number160.createHash(randomString(1));
+			Number160 contentKey = Number160.createHash(randomString(2));
+			Data versionA = new Data("versionA").addBasedOn(new Number160(0, Number160.ZERO)).protectEntry(keyPair1);
+			Data versionB = new Data("versionB").addBasedOn(new Number160(0, Number160.ONE)).protectEntry(keyPair1);
+			FuturePut putA = p1.put(locationKey).data(contentKey, versionA, Number160.ONE).keyPair(keyPair1).start()
+			        .awaitUninterruptibly();
+			Assert.assertTrue(putA.isSuccess());
+			Assert.assertFalse(hasVersionFork(putA));
+			// put version B where a version conflict should be detected because
+			// it
+			// is not based on version A
+			FuturePut putB = p1.put(locationKey).data(contentKey, versionB, Number160.ONE).keyPair(keyPair1).start()
+			        .awaitUninterruptibly();
+			Assert.assertTrue(hasVersionFork(putB));
+		} finally {
+			if (p1 != null) {
+				p1.shutdown().awaitUninterruptibly();
+			}
+			if (p2 != null) {
+				p2.shutdown().awaitUninterruptibly();
+			}
+		}
 	}
 
 	private static boolean hasVersionFork(FuturePut future) throws Exception {
